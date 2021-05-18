a4a27fd539b0c62e58c1a5172e3510c8aaf237de
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-05-18 21:17:32 +0300

    Day 6 lecture.

---
+++ hrms/src/main/java/kodlamaio/hrms/Api/Controllers/JobPositionsController.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.Api.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.Business.Abstract.JobPositionService;
+import kodlamaio.hrms.Entities.Concretes.JobPosition;
+
+
+@RestController 
+@RequestMapping("/api/jobpositions") //
+public class JobPositionsController {
+	
+	private JobPositionService jobPositionService;
+
+	@Autowired 
+	public JobPositionsController(JobPositionService jobPositionService) {
+		super();
+		this.jobPositionService = jobPositionService;
+	}
+	
+	@GetMapping("/getall")
+	public List<JobPosition> getAll(){
+		return jobPositionService.getAll();
+	}
+	
+}
