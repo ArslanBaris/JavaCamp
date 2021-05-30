1c807ff6ee1a9685f055fd332d3a27b5fb59c3b5
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-05-30 18:37:07 +0300

    refactor: Hrms revised.

--- hrms/src/main/java/kodlamaio/hrms/Api/Controllers/JobPositionsController.java
+++ hrms/src/main/java/kodlamaio/hrms/Api/Controllers/JobPositionsController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.Business.Abstract.JobPositionService;
+import kodlamaio.hrms.Core.Utilities.Results.Result;
 import kodlamaio.hrms.Entities.Concretes.JobPosition;
 
 
@@ -27,5 +30,7 @@ public class JobPositionsController {
 	public List<JobPosition> getAll(){
 		return jobPositionService.getAll();
 	}
+	
+	
 	
 }
