3af85b54cd47773769efce622efc1ad72ad93a57
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-04 00:06:18 +0300

    refactor: JobPosition revised.

--- hrms/src/main/java/kodlamaio/hrms/Entities/Concretes/JobPosition.java
+++ hrms/src/main/java/kodlamaio/hrms/Entities/Concretes/JobPosition.java
@@ -1,10 +1,13 @@
 package kodlamaio.hrms.Entities.Concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +30,8 @@ public class JobPosition {
 	@Column(name="title")
 	private String title;
 	
+	@OneToMany(mappedBy = "jobPosition")
+	private List<JobPosition> jobPositions;
 	
 	
 }
