f2e818a1afb944ec41598b09bb6e0d97e7ef7fd2
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-05 00:53:24 +0300

    refactor: .JobAdvertisementManager revised

--- hrms/src/main/java/kodlamaio/hrms/Business/Concretes/JobAdvertisementManager.java
+++ hrms/src/main/java/kodlamaio/hrms/Business/Concretes/JobAdvertisementManager.java
@@ -2,19 +2,24 @@ package kodlamaio.hrms.Business.Concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import kodlamaio.hrms.Business.Abstract.JobAdvertisementService;
 import kodlamaio.hrms.Core.Utilities.Results.DataResult;
+import kodlamaio.hrms.Core.Utilities.Results.ErrorResult;
 import kodlamaio.hrms.Core.Utilities.Results.Result;
 import kodlamaio.hrms.Core.Utilities.Results.SuccessDataResult;
 import kodlamaio.hrms.Core.Utilities.Results.SuccessResult;
 import kodlamaio.hrms.DataAccess.Abstract.JobAdvertisementsDao;
 import kodlamaio.hrms.Entities.Concretes.JobAdvertisements;
 
+@Service
 public class JobAdvertisementManager implements JobAdvertisementService {
 
 	private JobAdvertisementsDao jobAdvertisementsDao;
 	
-	
+	@Autowired
 	public JobAdvertisementManager(JobAdvertisementsDao jobAdvertisementsDao) {
 		super();
 		this.jobAdvertisementsDao = jobAdvertisementsDao;
@@ -39,9 +44,18 @@ public class JobAdvertisementManager implements JobAdvertisementService {
 	}
 
 	@Override
-	public Result changeOpentoClose(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Result changeToActive(int id) {
+		if(getById(id) == null) {
+			return new ErrorResult("Böyle bir iş ilanı yok!");
+		}
+		if (getById(id).getData().isActive() == false) {
+			return new SuccessResult("İş ilani zaten kapalı!");
+		
+		}
+		JobAdvertisements jobAdvertisements = getById(id).getData();
+		jobAdvertisements.setActive(false);
+		update(jobAdvertisements);
+		return new SuccessResult("İş ilanı başarılı şekilde kapatıldı!");
 	}
 
 	@Override
@@ -50,21 +64,24 @@ public class JobAdvertisementManager implements JobAdvertisementService {
 	}
 
 	@Override
-	public DataResult<List<JobAdvertisements>> getByIsActiveTrueOrderByJobListDateDesc() {
-		// TODO Auto-generated method stub
-		return null;
+	public DataResult<List<JobAdvertisements>> getByIsActiveTrueOrderByApplicationDeadlineDesc() {
+		return new SuccessDataResult<List<JobAdvertisements>>(this.jobAdvertisementsDao.findByIsActiveTrueOrderByApplicationDeadlineDesc());
 	}
 
 	@Override
 	public DataResult<List<JobAdvertisements>> getByIsActiveTrueAndEmployerId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return new SuccessDataResult<List<JobAdvertisements>>(this.jobAdvertisementsDao.findByIsActiveTrueAndEmployerId(id));
 	}
 
 	@Override
 	public DataResult<List<JobAdvertisements>> getAll() {
 		return new SuccessDataResult<List<JobAdvertisements>>(this.jobAdvertisementsDao.findAll());
 		
+	}
+
+	@Override
+	public DataResult<JobAdvertisements> getById(int id) {
+		return new SuccessDataResult<JobAdvertisements>(this.jobAdvertisementsDao.getOne(id));
 	}
 
 }
