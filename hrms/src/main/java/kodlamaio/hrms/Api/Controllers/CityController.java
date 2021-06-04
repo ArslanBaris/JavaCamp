fba135cdd0bf6977e8a19d3fa1023305ac9c67e7
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-04 16:29:31 +0300

    feat: CityController added.

---
+++ hrms/src/main/java/kodlamaio/hrms/Api/Controllers/CityController.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.Api.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.Business.Abstract.CityService;
+import kodlamaio.hrms.Core.Utilities.Results.DataResult;
+import kodlamaio.hrms.Entities.Concretes.City;
+
+@RestController
+@RequestMapping("/api/city")
+public class CityController {
+
+	private CityService cityService;
+
+	@Autowired
+	public CityController(CityService cityService) {
+		super();
+		this.cityService = cityService;
+	}
+	
+	@GetMapping("/getall")
+	public DataResult<List<City>> getAll(){
+		return this.cityService.getAll();
+	}
+	
+}
