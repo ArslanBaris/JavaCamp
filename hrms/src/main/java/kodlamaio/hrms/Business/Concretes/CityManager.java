fbc25301220d5869324c1780903402b0aa70378f
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-04 16:29:21 +0300

    feat: CityManager added.

---
+++ hrms/src/main/java/kodlamaio/hrms/Business/Concretes/CityManager.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.Business.Concretes;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.Business.Abstract.CityService;
+import kodlamaio.hrms.Core.Utilities.Results.DataResult;
+import kodlamaio.hrms.Core.Utilities.Results.SuccessDataResult;
+import kodlamaio.hrms.DataAccess.Abstract.CityDao;
+import kodlamaio.hrms.Entities.Concretes.City;
+
+@Service
+public class CityManager implements CityService {
+
+	private CityDao cityDao;
+	
+	
+	public CityManager(CityDao cityDao) {
+		super();
+		this.cityDao = cityDao;
+	}
+
+
+	@Override
+	public DataResult<List<City>> getAll() {
+		return new SuccessDataResult<List<City>>(this.cityDao.findAll(),"Data Listelendi");
+	}
+
+	
+}
