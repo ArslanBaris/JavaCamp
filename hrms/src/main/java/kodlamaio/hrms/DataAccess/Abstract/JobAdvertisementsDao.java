d6f9b4b1ccceb68d46fe3963822df7c395fbe397
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-05 00:53:10 +0300

    refactor: .JobAdvetisementDao revised

--- hrms/src/main/java/kodlamaio/hrms/DataAccess/Abstract/JobAdvertisementsDao.java
+++ hrms/src/main/java/kodlamaio/hrms/DataAccess/Abstract/JobAdvertisementsDao.java
@@ -3,17 +3,19 @@ package kodlamaio.hrms.DataAccess.Abstract;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.hrms.Entities.Concretes.JobAdvertisements;
 
 
 public interface JobAdvertisementsDao extends JpaRepository<JobAdvertisements, Integer> {
 
+	@Query("From JobAdvertisements where isActive = true")
 	List<JobAdvertisements> findByIsActiveTrue();
 	
-
-	List<JobAdvertisements> findByIsActiveTrueOrderByJobListDateDesc();
+	@Query("From JobAdvertisements where isActive = true Order By applicationDeadline Desc")
+	List<JobAdvertisements> findByIsActiveTrueOrderByApplicationDeadlineDesc();
 	
-
+	@Query("From JobAdvertisements where isActive = true and employer_id =:id")
 	List<JobAdvertisements> findByIsActiveTrueAndEmployerId(int id);
 }
