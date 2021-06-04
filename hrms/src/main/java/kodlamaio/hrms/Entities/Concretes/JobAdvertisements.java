7f364e3d4e9c9b34c22553ce82f2d8dd16ad8e1b
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-04 20:14:28 +0300

    refactor: JobAdvertisement revised.

--- hrms/src/main/java/kodlamaio/hrms/Entities/Concretes/JobAdvertisements.java
+++ hrms/src/main/java/kodlamaio/hrms/Entities/Concretes/JobAdvertisements.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
+import com.sun.istack.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -18,10 +22,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "job_advertisements")
-@PrimaryKeyJoinColumn(name = "id")
 
 public class JobAdvertisements {
-
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@NotNull
 	@Column(name = "job_detail")
 	private String jobDetail;
 	
@@ -31,6 +39,7 @@ public class JobAdvertisements {
 	@Column(name = "max_salary")
 	private double maxSalary;
 	
+	@NotNull
 	@Column(name = "position_quota")
 	private int positionQuota;
 	
@@ -47,10 +56,12 @@ public class JobAdvertisements {
 	@JoinColumn(name = "employer_id")
 	private Employers employer;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "city_id")
 	private City city;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "job_title_id")
 	private JobPosition jobPosition;
