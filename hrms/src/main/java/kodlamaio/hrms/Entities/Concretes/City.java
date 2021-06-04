1a4cb7ccf17b20a801f2e6bc580523d50964a4ba
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-05 00:52:17 +0300

    refactor: City revised.

--- hrms/src/main/java/kodlamaio/hrms/Entities/Concretes/City.java
+++ hrms/src/main/java/kodlamaio/hrms/Entities/Concretes/City.java
@@ -2,6 +2,7 @@ package kodlamaio.hrms.Entities.Concretes;
 
 import java.util.List;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,10 +11,13 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
 import lombok.NoArgsConstructor;
 
 @Data
@@ -21,17 +25,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "city")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","job_advertisements"})
 
 public class City {
 
 	@Id	
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //AutoIncrement 
 	@Column(name = "id")
 	private int id;
+	
 	
 	@Column(name = "city_name")
 	private String cityName;
 	
 	@OneToMany(mappedBy = "city")
 	private List<JobAdvertisements> advertisements;
+
+	public City( String cityName, List<JobAdvertisements> advertisements) {
+		super();
+		this.cityName = cityName;
+		this.advertisements = advertisements;
+	}
+	
 	
 }
