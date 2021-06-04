b703791283685f797c5223204ad214dc3812375c
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-06-05 00:53:32 +0300

    refactor: .CityManager revised

--- hrms/src/main/java/kodlamaio/hrms/Business/Concretes/CityManager.java
+++ hrms/src/main/java/kodlamaio/hrms/Business/Concretes/CityManager.java
@@ -2,6 +2,7 @@ package kodlamaio.hrms.Business.Concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.Business.Abstract.CityService;
@@ -15,7 +16,7 @@ public class CityManager implements CityService {
 
 	private CityDao cityDao;
 	
-	
+	@Autowired
 	public CityManager(CityDao cityDao) {
 		super();
 		this.cityDao = cityDao;
